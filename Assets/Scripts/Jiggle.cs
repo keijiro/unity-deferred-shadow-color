aee70d5b3f5c37786a8a64797da5a2832bc1b05b
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-10-08 16:52:47 +0900

    Initial commit.

---
+++ Assets/Scripts/Jiggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jiggle : MonoBehaviour
+{
+    public float amount = 5.0f;
+    public float freq = 0.5f;
+
+    Quaternion initialRotation;
+    float offset1;
+    float offset2;
+
+    void Awake ()
+    {
+        initialRotation = transform.localRotation;
+        offset1 = Random.value * 100.0f;
+        offset2 = Random.value * 100.0f;
+    }
+
+    void Update ()
+    {
+        transform.localRotation =
+            Quaternion.AngleAxis (amount * (Mathf.PerlinNoise (Time.time * freq + offset2, 0.0f) - 0.5f), Vector3.right) *
+            Quaternion.AngleAxis (amount * (Mathf.PerlinNoise (Time.time * freq + offset1, 0.0f) - 0.5f), Vector3.up) *
+            initialRotation;
+    }
+}
---
+++ Assets/Scripts/ShadowManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ShadowManager : MonoBehaviour {
+    public Color color;
+
+	void Update () {
+        Shader.SetGlobalColor ("_GlobalShadowColor", color);
+	}
+}
